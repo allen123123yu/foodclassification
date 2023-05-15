d0eaba65bed58b389e411fb626991946fcdfbfd4
allen123123yu <125625693+allen123123yu@users.noreply.github.com>
2023-05-16 05:06:17 +0800

    Add files via upload

---
+++ Food101Submission.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 04:30:50 2023
+
+@author: user
+"""
+
+import tensorflow as tf
+import matplotlib.image as img
+import numpy as np
+from collections import defaultdict
+import collections
+import os
+import matplotlib.pyplot as plt
+from shutil import copy
+from shutil import rmtree
+import tensorflow.keras.backend as K
+from tensorflow.keras import regularizers
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D, AveragePooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.regularizers import l2
+from tensorflow import keras
+import numpy as np
+
+#import food101 to directory
+#visualize the data to make sure it works
+os.listdir('food-101/images')
+rows = 17
+cols = 6
+fig, ax = plt.subplots(rows, cols, figsize=(25,25))
+fig.suptitle("Showing one random image from each class", y=1, fontsize=16) 
+data_dir = "food-101/images/"
+foods_sorted = sorted(os.listdir(data_dir))
+food_id = 0
+for i in range(rows):
+  for j in range(cols):
+    try:
+      food_selected = foods_sorted[food_id] 
+      food_id += 1
+    except:
+      break
+    food_selected_images = os.listdir(os.path.join(data_dir,food_selected)) 
+    food_selected_random = np.random.choice(food_selected_images) 
+    img = plt.imread(os.path.join(data_dir,food_selected, food_selected_random))
+    ax[i][j].imshow(img)
+    ax[i][j].set_title(food_selected, pad = 10)
+plt.setp(ax, xticks=[],yticks=[])
+plt.tight_layout()
+
+#split the dataset into train and test folders: input your own paths
+def prepare_data(filepath, src,dest):
+  classes_images = defaultdict(list)
+  with open(filepath, 'r') as txt:
+      paths = [read.strip() for read in txt.readlines()]
+      for p in paths:
+        food = p.split('/')
+        classes_images[food[0]].append(food[1] + '.jpg')
+
+  for food in classes_images.keys():
+    print("\nCopying images into ",food)
+    if not os.path.exists(os.path.join(dest,food)):
+      os.makedirs(os.path.join(dest,food))
+    for i in classes_images[food]:
+      copy(os.path.join(src,food,i), os.path.join(dest,food,i))
+      
+#copy the train data
+prepare_data('food-101/meta/train.txt', 'food-101/images', 'food-101/train')
+
+#copy the test data
+prepare_data('food-101/meta/test.txt', 'food-101/images', 'food-101/test')
+
+#create train and test samples
+
+def dataset_mini(food_list, src, dest):
+  os.makedirs(dest)
+  for food_item in food_list :
+    print("Copying images into",food_item)
+    copytree(os.path.join(src,food_item), os.path.join(dest,food_item))
+
+#picked 3 food items for train and test
+food_list = ['Bibimbap','Ceviche','Cheesecake']
+src_train = 'food-101/train'
+dest_train = 'food-101/train_mini'
+src_test = 'food-101/test'
+dest_test = 'food-101/test_mini'
+
+#3 food item train and test      
+dataset_mini(food_list, src_train, dest_train)
+
+dataset_mini(food_list, src_test, dest_test)
+
+#using pretrained model inceptionv3
+K.clear_session()
+
+n_classes = 3
+img_width, img_height = 299, 299
+train_data_dir = 'food-101/train_mini'
+validation_data_dir = 'food-101/test_mini'
+nb_train_samples = 2250 #75750
+nb_validation_samples = 750 #25250
+batch_size = 16
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+
+inception = InceptionV3(weights='imagenet', include_top=False)
+x = inception.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(128,activation='relu')(x)
+x = Dropout(0.2)(x)
+
+predictions = Dense(3,kernel_regularizer=regularizers.l2(0.005), activation='softmax')(x)
+
+model = Model(inputs=inception.input, outputs=predictions)
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+checkpointer = ModelCheckpoint(filepath='best_model_3class.hdf5', verbose=1, save_best_only=True)
+csv_logger = CSVLogger('history.log')
+
+history = model.fit_generator(train_generator,
+                    steps_per_epoch = nb_train_samples // batch_size,
+                    validation_data=validation_generator,
+                    validation_steps=nb_validation_samples // batch_size,
+                    epochs=10,
+                    verbose=1,
+                    callbacks=[csv_logger, checkpointer])
+
+model.save('model_trained_3class.hdf5')
+
+#plot the accuracy and loss
+def plot_accuracy(history,title):
+    plt.title(title)
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train_accuracy', 'validation_accuracy'], loc='best')
+    plt.show()
+def plot_loss(history,title):
+    plt.title(title)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train_loss', 'validation_loss'], loc='best')
+    plt.show()
+
+
+plot_accuracy(history,'FOOD101-Inceptionv3')
+plot_loss(history,'FOOD101-Inceptionv3')
+
+
+
+
+
+
